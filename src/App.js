457d751436859b8f20fddaf7307cda2d372c50b5
ngoctan1234 <ngoctannew@gmail.com>
2024-09-10 16:25:11 +0700

    ud1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { BrowserRouter as Router, Routes, Route ,Link, BrowserRouter} from "react-router-dom";
+import HomePage from './pages/home/HomePage';
+import StudentPage from './pages/student/StudentPage';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route index element={<HomePage/>}></Route>
+          <Route path='/student' element={<StudentPage />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
